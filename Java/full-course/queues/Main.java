e57c05a2ae781b48abf01f543c1e5c42bb80a866
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-24 19:20:16 -0400

    resolved misunderstanding about implementation class based on queue interface

--- Java/full-course/queues/Main.java
+++ Java/full-course/queues/Main.java
@@ -1,11 +1,15 @@
-// priority queue ---> FIFO
-//                ---> serves highest priority elements before low priority
+// regular queue ---> FIFO
+// priority queue ---> serves highest priority elements before low priority
+// In java Queue class is an interface/Abstract base class
+// based on the collections heirarchy LinkedList and Priority Queues implement a queue but priority queue uses a scheme to adjust elements based on priority.
+// we use linked list class which defines the Queue interface to implement regular queues
 import java.util.*;
 
 public class Main{
     public static void main(String[] args)
     {
         Queue<Double> queue = new LinkedList<>();
+        // offer, poll, peek
         queue.offer(3.14);
         queue.offer(4.89);
         queue.offer(10.97);
@@ -14,7 +18,7 @@ public class Main{
         System.out.println(queue);
         System.out.println(queue.peek());
 
-        queue.remove();
+        queue.poll();
         System.out.println(queue);
 
 
