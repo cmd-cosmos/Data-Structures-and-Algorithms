88c986dccce115bc9c42a3fabfc53c9d54b89d76
Premanshu Bhagat <premtsapj@gmail.com>
2025-09-24 14:37:30 -0400

    added search and size operations for the stack

--- Java/full-course/stacks/Main.java
+++ Java/full-course/stacks/Main.java
@@ -22,7 +22,19 @@ public class Main{
 
         // popping val from top of stack and seeing val on top by peeking 
         stack.pop(); // should pop "Robin"
-        System.out.println("Val on top post pop(): " + stack.peek());
+        System.out.println("Val on top post pop(): " + stack.peek() + '\n');
+
+        // searching for a val in the stack
+        // .search() returns depth from top so subtract from size of the stack
+        String search_val = "Batman";
+        System.err.println("Searching for Batman object on the stack...");
+        get_size(stack);
+        System.out.println("Object found at Index: " + (stack.size() - stack.search(search_val)) + '\n');
+
+        String search_val_2 = "Alfred";
+        System.err.println("Searching for Alfred object on the stack...");
+        get_size(stack);
+        System.out.println("Object found at Index: " + (stack.size() - stack.search(search_val_2)));
 
         }
     public static void check_empty(Stack<String> stack_instance)
@@ -30,5 +42,10 @@ public class Main{
         // checking if stack is empty
         boolean flag = stack_instance.empty();
         System.out.println(("Stack empty flag: " + flag + '\n'));
+    }
+    public static void get_size(Stack<String> stack_instance)
+    {
+        int stack_size = stack_instance.size();
+        System.out.println("Stack Size: " + stack_size);
     }
 }
