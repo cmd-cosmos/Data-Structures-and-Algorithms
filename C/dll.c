0f08fa10225436e990933eaf91f951f3ab355c05
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-14 19:00:48 -0400

    init commit - basic doubly linked list implementation

---
+++ C/dll.c
@@ -0,0 +1,23 @@
+// doubly linked lists
+
+typedef struct node node_t;
+
+struct node
+{
+    int val;
+    node_t* prev;
+    node_t* next;
+};
+
+
+int main()
+{
+    node_t n1, n2, n3;
+    node_t* head;
+    node_t* tail;
+
+    tail->next = &head;
+    
+
+    return 0;
+}
