a7d4012b33a45e16549b3dac1f181bfc12000f9b
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-16 22:14:12 -0400

    basic tree with preorder traversal

---
+++ C/basic/tree.c
@@ -0,0 +1,54 @@
+/*
+target tee:
+        1
+       / \
+      2   3
+     / \   
+    4   5
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int value;
+    struct node* left_child;
+    struct node* right_child;
+}node_t;
+
+node_t* new_Node(int val)
+{
+    node_t* newNode = (node_t*)malloc(sizeof(node_t));
+    newNode->value       = val;
+    newNode->left_child  = NULL;
+    newNode->right_child = NULL;
+
+    return newNode;
+}
+
+void preorder_traverse(node_t* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    printf("%d -> ", root->value);
+    preorder_traverse(root->left_child);
+    preorder_traverse(root->right_child);
+}
+
+
+int main()
+{
+    node_t* root = new_Node(1);
+    root->left_child = new_Node(2);
+    root->right_child = new_Node(3);
+    root->left_child->left_child = new_Node(4);
+    root->left_child->right_child = new_Node(5);
+
+    printf("preorder traversal: ");
+    preorder_traverse(root);
+
+    return 0;
+}
