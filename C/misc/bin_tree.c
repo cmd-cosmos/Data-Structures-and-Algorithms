7eabdc2e30682180e86bee9b2a3732dae39522b0
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-29 23:53:18 -0400

    implementing a binary tree

---
+++ C/misc/bin_tree.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node node_t;
+
+struct Node
+{
+    int val;
+    node_t* left;
+    node_t* right;
+};
+
+node_t* newNode(int data)
+{
+    node_t* newNode = (node_t*)malloc(sizeof(node_t));
+    newNode->val   = data;
+    newNode->left  = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+node_t* recursiveInsert(node_t* root, node_t* newNode)
+{
+    if (root == NULL)
+    {
+        return newNode;
+    }
+    if (newNode->val <= root->val)
+    {
+        root->left = recursiveInsert(root->left, newNode);
+    }
+    else
+    {
+        root->right = recursiveInsert(root->right, newNode);
+    }
+    return root;
+}
+
+
+int main()
+{
+    node_t* rootNode = newNode(10);
+    node_t* a = newNode(10);
+    node_t* b = newNode(20);
+    node_t* c = newNode(30);
+    node_t* d = newNode(40);
+    node_t* e = newNode(50);
+
+    return 0;
+}
+
