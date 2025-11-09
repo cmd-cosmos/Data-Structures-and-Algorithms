7d907a822b5dc349ca0de4c9a200ad57628afa08
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-09 10:45:15 -0500

    dequeue

---
+++ Python/misc/rough/q.py
@@ -0,0 +1,30 @@
+#pylint: skip-file
+
+# using lists as queues causes expensive dequeue for very large queues.
+# better to use deque --> double ended queue
+# internally is a block linked list
+# <block 1> --- <block 2> ... --> blocks are arrays 
+# linked list of arrays
+# combines best of both worlds --> cache locality of arrays and quick inserts and pops of linked lists 
+
+from collections import deque
+
+def draw_q(q):
+    print("queue: ")
+    print(" -> ".join(q))
+
+q = deque()
+
+# enequeue --> O(1)
+q.append('A')
+q.append('B')
+q.append('C')
+q.append('D')
+
+draw_q(q)
+
+q.popleft() # dequeue --> O(1)
+draw_q(q)
+
+# peek --> O(1)
+print("peek: ",q[0])
