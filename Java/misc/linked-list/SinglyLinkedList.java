0079732679cde81a6189c3c062d75726482fa87f
Premanshu Bhagat <premtsapj@gmail.com>
2026-01-08 14:38:31 -0500

    added constructor to the ll

---
+++ Java/misc/linked-list/SinglyLinkedList.java
@@ -0,0 +1,11 @@
+import java.util.NoSuchElementException;
+
+public class SinglyLinkedList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public SinglyLinkedList() {
+        // empty
+    }
+}
