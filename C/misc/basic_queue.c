7f1de7cbc98592e7ae0430cbc6a18f34db5608a2
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-04 14:14:49 -0500

    added definition for the enqueue method

--- C/misc/basic_queue.c
+++ C/misc/basic_queue.c
@@ -5,7 +5,7 @@
 
 #define MAX_ELEMENTS 100
 int intQueue[MAX_ELEMENTS];
-int front = 0;
+int front = -1;
 int rear  = -1;
 int count = 0;
 
@@ -14,7 +14,6 @@ bool isFull();
 bool enqueue(int data);
 bool dequeue();
 void printQueue(int* start);
-int size();
 
 int main(void)
 {
@@ -35,4 +34,22 @@ bool isEmpty()
 bool isFull()
 {
     return count == MAX_ELEMENTS;
+}
+
+bool enqueue(int data)
+{
+    if (rear == MAX_ELEMENTS - 1)
+    {
+        puts("Overflow Detected");
+    }
+    else
+    {
+        if (front == -1)
+        {
+            front = 0;
+        }
+        rear++;
+        intQueue[rear] = data;
+        printf("%d added to queue.\n", data);
+    }
 }
