7d770a36df98f0780b881a358c70bfec8723a55c
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-04 13:11:11 -0500

    added some globals

--- C/misc/basic_queue.c
+++ C/misc/basic_queue.c
@@ -1,11 +1,20 @@
+// array repr of a queue
+
 #include <stdio.h>
 #include <stdbool.h>
+
+#define MAX_ELEMENTS 100
+int intQueue[MAX_ELEMENTS];
+int front = 0;
+int rear  = -1;
+int count = 0;
 
 bool isEmpty();
 bool isFull();
 bool enqueue(int data);
 bool dequeue();
 void printQueue(int* start);
+int size();
 
 int main(void)
 {
