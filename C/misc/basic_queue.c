de6dbc12772cb642d983e3fc1db7999761634c54
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-04 11:20:17 -0500

    added function prototypes for common queue methods

---
+++ C/misc/basic_queue.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isEmpty();
+bool isFull();
+bool enqueue(int data);
+bool dequeue();
+void printQueue(int* start);
+
+int main(void)
+{
+    return 0;
+}
